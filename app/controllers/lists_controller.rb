c151b19bd1c72a510c93373e558c6c0eba5d7ba5
Carine Czodor <caamlo@hotmail.com>
2025-03-27 16:52:04 +0100

    controller bookmark et list ok

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,32 @@
 class ListsController < ApplicationController
-end
+   # Action pour afficher toutes les listes
+   before_action :authenticate_user!
+
+   def index
+    @lists = current_user.lists
+   end
+
+   def show
+     @list = current_user.lists.find(params[:id])
+   end
+
+   def new
+    @list = List.new
+   end
+
+   def create
+     @list = current_user.lists.build(list_params)
+
+     if @list.save
+       redirect_to @list, notice: 'La liste a été créée avec succès.'
+     else
+       render :new
+     end
+   end
+
+   private
+
+   def list_params
+     params.require(:list).permit(:name, :photo)
+   end
+ end
