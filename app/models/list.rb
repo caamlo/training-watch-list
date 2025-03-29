5889b876b7dc6d375cea4c533967da1889265637
caamlo <caamlo@hotmail.com>
2025-03-29 10:39:05 +0100

    Update list.rb

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,6 +2,6 @@ class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
   belongs_to :user
-  has_one_attached :photo 
+  #has_one_attached :photo 
   validates :name, presence: true, uniqueness: true
 end
